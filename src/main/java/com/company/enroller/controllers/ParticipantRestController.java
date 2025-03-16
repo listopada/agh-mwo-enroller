ab875553624015b0ecb9ec26323ea254d450ce83
Ania <listopad@agh.edu.pl>
2025-03-16 19:38:23 +0100

    Update

--- src/main/java/com/company/enroller/controllers/ParticipantRestController.java
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -5,9 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.ParticipantService;
@@ -19,10 +17,82 @@ public class ParticipantRestController {
 	@Autowired
 	ParticipantService participantService;
 
+//	@RequestMapping(value = "", method = RequestMethod.GET)
+//	public ResponseEntity<?> getParticipants() {
+//		Collection<Participant> participants = participantService.getAll();
+//		return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
+//	}
+
 	@RequestMapping(value = "", method = RequestMethod.GET)
-	public ResponseEntity<?> getParticipants() {
-		Collection<Participant> participants = participantService.getAll();
+	public ResponseEntity<?> getParticipants(
+			@RequestParam(value = "sortBy", defaultValue = "login") String sortBy,
+			@RequestParam(value = "sortOrder", defaultValue = "ASC") String sortOrder) {
+
+		if (!sortOrder.equalsIgnoreCase("ASC") && !sortOrder.equalsIgnoreCase("DESC")) {
+			return new ResponseEntity<String>("Invalid sortOrder value. Allowed values are 'ASC' or 'DESC'.", HttpStatus.BAD_REQUEST);
+		}
+
+		Collection<Participant> participants = participantService.getAllSorted(sortBy, sortOrder);
 		return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getParticipant(@PathVariable("id") String login) {
+		Participant participant = participantService.findByLogin(login);
+		if (participant == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+	}
+
+
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<?> registerParticipant(@RequestBody Participant participant) {
+		Participant existingParticipant = participantService.findByLogin(participant.getLogin());
+
+		if (existingParticipant != null) {
+			return new ResponseEntity<String>(
+					"Unable to create user: " + participant.getLogin() + ", User already exists.",
+					HttpStatus.CONFLICT);
+		}
+
+		participantService.add(participant);
+		return new ResponseEntity<String>(
+				"User with login '" + participant.getLogin() + "' has been successfully created.",
+				HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "/{login}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> deleteParticipant(@PathVariable("login") String login) {
+		Participant participant = participantService.findByLogin(login);
+		if (participant == null) {
+			return new ResponseEntity<String>(
+					"Unable to delete. Participant with login " + login + " not found.",
+					HttpStatus.NOT_FOUND);
+		}
+
+		participantService.delete(participant);
+		return new ResponseEntity<String>(
+				"User with login '" + login + "' has been deleted.",
+				HttpStatus.OK);
+	}
+
+
+	@RequestMapping(value = "/{login}", method = RequestMethod.PUT)
+	public ResponseEntity<?> updateParticipant(@PathVariable("login") String login, @RequestBody Participant updatedParticipant) {
+		Participant existingParticipant = participantService.findByLogin(login);
+		if (existingParticipant == null) {
+			return new ResponseEntity<String>(
+					"Unable to update the participant. Participant with login " + login + " has not been found.",
+					HttpStatus.NOT_FOUND);
+		}
+
+		existingParticipant.setPassword(updatedParticipant.getPassword());
+		participantService.update(existingParticipant);
+
+		return new ResponseEntity<String>(
+				"User with login '" + login + "' has been successfully updated.",
+				HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -22,4 +23,34 @@ public class ParticipantService {
 		return query.list();
 	}
 
+	public Collection<Participant> getAllSorted(String sortBy, String sortOrder) {
+		String hql = "FROM Participant ORDER BY " + sortBy + " " + sortOrder;
+		Query query = connector.getSession().createQuery(hql);
+		return query.list();
+	}
+
+	public Participant findByLogin(String login) {
+		String hql = "FROM Participant WHERE login = :login";
+		Query query = connector.getSession().createQuery(hql);
+		query.setParameter("login", login);
+		return (Participant) query.uniqueResult();
+	}
+
+	public void add(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+	}
+
+	public void delete(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();
+	}
+
+	public void update(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().update(participant);
+		transaction.commit();
+	}
 }
